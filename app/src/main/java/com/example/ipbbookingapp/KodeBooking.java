f2eb97b2064e693d24a625d82b14888c63c9af84
nisrinasudrajat <nisrina_sudrajat@apps.ipb.ac.id>
2019-04-22 14:35:02 +0700

    layout kode&riwayat

---
+++ app/src/main/java/com/example/ipbbookingapp/KodeBooking.java
@@ -0,0 +1,13 @@
+package com.example.ipbbookingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KodeBooking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kode_booking);
+    }
+}
